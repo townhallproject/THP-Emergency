90ac4fdf67b4094e0d452cee66bdf3f302a559ba
meganrm <meganrm@gmail.com>
2019-10-12 18:02:28 -0700

    factored out constants and updated values

---
+++ src/constants/index.js
@@ -0,0 +1,32 @@
+// Static Dicts
+export const responseDict = {
+    1: 'Full support for impeachment',
+    2: 'Supports impeachment inquiry',
+    3: 'No position / Unclear',
+    4: 'Expressed concerns',
+    5: 'Opposed to impeachment'
+}
+
+export const responseDictPopover = {
+    1: 'supports impeachment',
+    2: 'supports inquiry',
+    3: 'unclear',
+    4: 'concerned',
+    5: 'opposed',
+}
+
+export const responseClass = {
+    1: 'impeachment',
+    2: 'inquiry',
+    3: 'unclear',
+    4: 'concerned',
+    5: 'opposed',
+}
+
+export const mapColors = {
+    1: '#542788',
+    2: '#998ec3',
+    3: '#c6c6c6',
+    4: '#f1a340',
+    5: '#b35806',
+}
